ef3c282197e599f35590ceff445a4212913d179a
Dasom Heo <dasomee@outlook.com>
2023-03-12 19:49:44 -0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const getAdvice = () => {
+    fetch("https://api.adviceslip.com/advice")
+    .then ((response) => response.json())
+    .then ((data) => {
+        slip = data.slip
+    })
+    .then (() => {
+        const adviceID = document.querySelector('.advice-id')
+        const adviceContent = document.querySelector('.advice-content')
+        adviceID.textContent = slip.id
+        adviceContent.textContent = slip.advice
+    })
+}
+
+const button = document.querySelector('button')
+button.addEventListener('click', () => {
+    getAdvice()
+})
+
+getAdvice();
